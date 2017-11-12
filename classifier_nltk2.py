84030614bef97a0d1730a43900587a2f7a15064c
Dheeraj Singh Patel <patel.drj@gmail.com>
2017-11-13 00:42:30 +0530

    Update classifier_nltk2.py

--- classifier_nltk2.py
+++ classifier_nltk2.py
@@ -13,29 +13,6 @@ from nltk.classify import ClassifierI
 from statistics import mode
 from sklearn.externals import joblib
 
-
-class VoteClassifier(ClassifierI):
-    def __init__(self, *classifiers):
-        self._classifiers = classifiers
-
-    def classify(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-        return mode(votes)
-
-    def confidence(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-
-        choice_votes = votes.count(mode(votes))
-        conf = choice_votes / len(votes)
-        return conf
-
-
 stop = stopwords.words('english') + list(string.punctuation)
 
 short_pos = open(os.getcwd() + '/data/positive.txt', 'r').read()
@@ -94,44 +71,3 @@ print('original accuracy : {}'.format(nltk.classify.accuracy(classifier ,testing
 classifier.show_most_informative_features(15)
 with open(os.getcwd() + "/data/original_classifier.pickle","wb") as f:
     joblib.dump(classifier, f)
-
-
-# print('classification in process for MNB_classifier')
-# MNB_classifier = SklearnClassifier(MultinomialNB())
-# MNB_classifier.train(training_set)
-# print('MNB_classifier accuracy : {}'.format(nltk.classify.accuracy(MNB_classifier ,testing_set)*100))
-# with open("MNB_classifier.pickle","wb") as f:
-#     joblib.dump(MNB_classifier, f)
-
-# print('classification in process for LogisticRegression_classifier')
-# LogisticRegression = SklearnClassifier(LogisticRegression())
-# LogisticRegression.train(training_set)
-# print('LogisticRegression accuracy : {}'.format(nltk.classify.accuracy(LogisticRegression ,testing_set)*100))
-# with open("LogisticRegression.pickle","wb") as f:
-#     joblib.dump(LogisticRegression, f)
-
-# print('classification in process for SGDclassifier')
-# SGDClassifier = SklearnClassifier(SGDClassifier())
-# SGDClassifier.train(training_set)
-# print('SGDClassifier accuracy : {}'.format(nltk.classify.accuracy(SGDClassifier ,testing_set)*100))
-# with open("SGDClassifier.pickle","wb") as f:
-#     joblib.dump(SGDClassifier, f)
-
-# print('classification in process for LinearSVC_classifier')
-# LinearSVC = SklearnClassifier(LinearSVC())
-# LinearSVC.train(training_set)
-# print('LinearSVC accuracy : {}'.format(nltk.classify.accuracy(LinearSVC ,testing_set)*100))
-# with open("LinearSVC.pickle","wb") as f:
-#     joblib.dump(LinearSVC, f)
-
-# print('classification in process for voted_classifier')
-# voted_classifier = VoteClassifier(classifier,
-#                                   LinearSVC,
-#                                   SGDClassifier,
-#                                   MNB_classifier,
-#                                   LogisticRegression)
-
-# print("voted_classifier accuracy percent:", (nltk.classify.accuracy(voted_classifier, testing_set))*100)
-
-# print("Classification:", voted_classifier.classify(testing_set[0][0]), "Confidence %:",voted_classifier.confidence(testing_set[0][0])*100)
-
